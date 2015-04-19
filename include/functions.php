960aab5304db86716574ca1ec707633d33b7745a
DenisVS <denvsim@gmail.com>
2015-04-19 17:14:00 +0000

    Запрос диреткории обработанных файлов, чтение содержимого исходника

--- include/functions.php
+++ include/functions.php
@@ -1,44 +1,45 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-function getFileList($dir, $recurse=false)
-  {
-    // массив, хранящий возвращаемое значение
-    $retval = array();
+function getFileList($dir, $recurse = false) {
+  // массив, хранящий возвращаемое значение
+  $retval = array();
 
-    // добавить конечный слеш, если его нет
-    if(substr($dir, -1) != "/") $dir .= "/";
+  // добавить конечный слеш, если его нет
+  if (substr($dir, -1) != "/")
+    $dir .= "/";
 
-    // указание директории и считывание списка файлов
-    $d = @dir($dir) or die("getFileList: Не удалось открыть каталог $dir для чтения");
-    while(false !== ($entry = $d->read())) {
+  // указание директории и считывание списка файлов
+  $d = @dir($dir) or die("getFileList: Не удалось открыть каталог " . $dir . " для чтения\n");
+  while (false !== ($entry = $d->read())) {
 
-      // пропустить скрытые файлы
-      if($entry[0] == ".") continue;
-      if(is_dir("$dir$entry")) {
-        $retval[] = array(
-          "name" => "$dir$entry/",
-          "size" => 0,
-          "lastmod" => filemtime("$dir$entry")
-        );
-        if($recurse && is_readable("$dir$entry/")) {
-          $retval = array_merge($retval, getFileList("$dir$entry/", true));
-        }
-      } elseif(is_readable("$dir$entry")) {
-        $retval[] = array(
-          "name" => "$dir$entry",
-          "size" => filesize("$dir$entry"),
-          "lastmod" => filemtime("$dir$entry")
-        );
+    // пропустить скрытые файлы
+    if ($entry[0] == ".")
+      continue;
+    if (is_dir("$dir$entry")) {
+      $retval[] = array(
+        "name" => "$dir$entry/",
+        "size" => 0,
+        "lastmod" => filemtime("$dir$entry")
+      );
+      if ($recurse && is_readable("$dir$entry/")) {
+        $retval = array_merge($retval, getFileList("$dir$entry/", true));
       }
     }
-    $d->close();
-
-    return $retval;
+    elseif (is_readable("$dir$entry")) {
+      $retval[] = array(
+        "name" => "$dir$entry",
+        "size" => filesize("$dir$entry"),
+        "lastmod" => filemtime("$dir$entry")
+      );
+    }
   }
-  
+  $d->close();
+
+  return $retval;
+}
--- index.php
+++ index.php
@@ -1,32 +1,64 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /* на будущее, блок подключение файлов в директории
-$includeDir = "include/functions";
-$dh = opendir($includeDir);
-while ($filename = readdir($dh)) {
+  $includeDir = "include/functions";
+  $dh = opendir($includeDir);
+  while ($filename = readdir($dh)) {
   $filename = $includeDir . "/" . $filename;
   include_once($filename);
-}
-*/
+  }
+ */
 
 
 if (isset($argv[1])) {
-	$workdir = $argv[1];
-    echo 'Обрабатываем директорию ' . $workdir ."\n";
-}	else
-{
-	echo "Введите директорию с файлами\n";
-
+  $indir = $argv[1];
+  echo 'Директория исходных файлов ' . $indir . "\n";
 }
+else {
+  echo "Введите директорию с исходными файлами\n";
+  exit();
+}
+
+if (isset($argv[2])) {
+  $outdir = $argv[2];
+  echo 'Директория с обработанными файлами ' . $outdir . "\n";
+}
+else {
+  echo "Введите директорию с обработанными файлами\n";
+  exit();
+}
+
 
 include 'include/functions.php';
 
-print_r (getFileList ($workdir, TRUE));
+$sourceFiles = (getFileList($indir, TRUE));
 
-  
+//цикл вывода всего массива непустых файлов
+for ($i = 0; $i < count($sourceFiles); $i++) {
+  if ($sourceFiles[$i]['size'] > 0) {
+    echo "Обрабатывается " . $sourceFiles[$i]['name'] . "\n";
+
+    $textFile = fopen($sourceFiles[$i]['name'], "r");  //открываем файл на чтение
+    if (!$textFile) {  //если файл не открывается
+      echo "\nОшибка открытия файла!\n";
+      exit(); //выходим
+    }
+    //читаем файл
+    $count = 0;
+    while (!feof($textFile)) {  //пока не дошли до конца файла 
+      $aText[$count] = fgets($textFile);
+      echo $aText[$count];
+      echo "\n";
+      $count = $count + 1;
+    }
+
+    fclose($textFile); //закрываем
+  }
+}
+

3da239412fd3f06592911695ca0b7e27e43076a6
DenisVS <denvsim@gmail.com>
2015-04-20 09:47:11 +0000

    Пути и слэши отлажены

--- index.php
+++ index.php
@@ -18,13 +18,16 @@
 include 'include/functions.php';
 
 if (isset($argv[1])) {
-  $lenghtInPath = mb_strlen($argv[1]) - 1;
-  $pos = mb_strpos($argv[1], "/", $lenghtInPath); // Есть ли в последней позиции слэш
+  $lenghtInPath = mb_strlen($argv[1]);
+  $pos = mb_strpos($argv[1], "/", $lenghtInPath - 1); // Есть ли в последней позиции слэш
+  echo $lenghtInPath . "  это введённая длина исходной директории\n";
+  echo $pos . "  это позиция слэша\n";
   if ($pos === false) {
     $inDir = $argv[1];
   }
   else {
-    $inDir = substr($argv[1], 0, $lenghtInPath);    // отрубаем последний слэш     
+    $inDir = substr($argv[1], 0, $lenghtInPath - 1);    // отрубаем последний слэш  
+    $lenghtInPath = $lenghtInPath - 1; // и длину приводим в соответствии с действительностью
   }
   echo 'Директория исходных файлов ' . $inDir . "\n";
 }
@@ -34,13 +37,16 @@ else {
 }
 
 if (isset($argv[2])) {
-  $lenghtOutPath = mb_strlen($argv[2]) - 1;
-  $pos = mb_strpos($argv[2], "/", $lenghtOutPath); // Есть ли в последней позиции слэш
+  $lenghtOutPath = mb_strlen($argv[2]);
+  $pos = mb_strpos($argv[2], "/", $lenghtOutPath - 1); // Есть ли в последней позиции слэш
+  echo $lenghtOutPath . "  это введённая длина целевой директории\n";
+  echo $pos . "  это позиция слэша\n";
   if ($pos === false) {
     $outDir = $argv[2];
   }
   else {
-    $outDir = substr($argv[2], 0, $lenghtOutPath);    // отрубаем последний слэш     
+    $outDir = substr($argv[2], 0, $lenghtOutPath - 1);    // отрубаем последний слэш     
+    $lenghtOutPath = $lenghtOutPath - 1; // и длину приводим в соответствии с действительностью
   }
   mkdir($outDir, 0755, true);
   echo 'Директория с обработанными файлами ' . $outDir . "\n";
@@ -49,27 +55,38 @@ else {
   echo "Введите директорию с обработанными файлами\n";
   exit();
 }
-
+echo "---\n";
 $sourceFiles = (getFileList($inDir, TRUE)); // получаем листинг
 //цикл вывода всего массива непустых файлов
 for ($i = 0; $i < count($sourceFiles); $i++) {
   if ($sourceFiles[$i]['size'] > 0) {
     echo "Обрабатывается " . $sourceFiles[$i]['name'] . "\n";
     $contentInFile = file_get_contents($sourceFiles[$i]['name']); // дёргаем контент целиком
-
-    
-    echo $contentInFile."\n";
-
-    $outFilePath = $outDir . "/" . mb_substr($sourceFiles[$i]['name'], $lenghtOutPath);
+    //echo $contentInFile."\n";
+    echo $lenghtInPath . ' Файл из массива ' . $sourceFiles[$i]['name'] . "\n";
+    $outFilePath = $outDir . "/" . mb_substr($sourceFiles[$i]['name'], $lenghtInPath + 1);
     echo "Путь целевого файла " . $outFilePath . "\n";
 
-    $targetFile = fopen($outFilePath,'a') or die("can't open file");
+    //$targetFile = fopen($outFilePath,'a') or die("can't open file");
     //fwrite($targetFile, ); //выводим в файл
 
 
 
     fclose($textFile); //закрываем
     echo "-------------------------------------------------\n";
+  }
+  else {
+    //Если нулевой дины, проверяем, директория ли
+    $pos = mb_strpos($sourceFiles[$i]['name'], "/", $lenghtOutPath); // Есть ли в последней позиции слэш
+    if ($pos === false) {
+      echo "Это файл нулевой длины\n";
+    }
+    else {
+      echo $lenghtInPath . ' Директория из массива ' . $sourceFiles[$i]['name'] . "\n";
+      // если же директория
+      $outDirPath = $outDir . "/" . mb_substr($sourceFiles[$i]['name'], $lenghtInPath + 1);
+      echo "Путь целевой директории " . $outDirPath . "\n";
+    }
   }
 }
 
